923cd1ade8c2f80c877f10b29a7cdc19c48e2d30
xavier.grimaldi@live.fr <Vous@exemple.com>
2019-03-25 01:44:41 +0100

    Ajout des fonctions de tests includes et de sa fonction

--- src/interval.test.js
+++ src/interval.test.js
@@ -80,3 +80,77 @@ describe('\n#########################################\n'+
 });
 
 
+/**
+ * Test de la fonction includes() du fichier 'interval.js'
+ */
+describe('\n#########################################\n'+
+         'Test de la fonction "includes()"\n'+
+         '#########################################', function () {
+
+    // #####################################
+    // Non-Including interval
+    // #####################################
+    // Vérification avec différents type d'intervalles positifs, négatifs, continus (se suivant l'un l'autre), données non numérique
+    let inter2n = new Interval(-15,-25);
+    let inter1n = new Interval(-5,-10);
+    let inter1 = new Interval(5,17);
+    let inter2 = new Interval(15,25);
+    
+    let interContinu1n = new Interval(-10,0);
+    let interContinu1 = new Interval(0,10);
+    let interContinu2 = new Interval(10,20);
+
+    let interAscii1 = new Interval('a','z');
+    let interAscii2 = new Interval('b','g');
+    let interAscii3 = new Interval('a','b');
+    let interAscii4 = new Interval('c','g');
+    let interEmpty= new Interval ('','');
+    let interBool= new Interval (true,false);
+
+
+    test.each([
+        [inter1, inter2,false,""],
+        [inter2, inter1,false,""],
+        [inter2n, inter1n,false,""],
+        [inter1n, inter2n,false,""],
+        [inter1, inter1n,false,""],
+        [interContinu1n, interContinu1,false,"Test continue entre une variable positive et négative."],
+        [interContinu1, interContinu2,false,"Test continue entre deux variables positives."],
+        [interAscii3, interAscii4,false,"Test avec des caractères ascii"],
+        [interAscii3, interEmpty,false,"Test avec des caractères ascii, et caractère vide"],
+        [interAscii3, interBool,false,"Test avec des caractères ascii, et caractère booléen"],
+      ])(
+        'Test Non-Including: (%s includes %s) = expected(%s). %s',
+        (int1,int2,expected ) => {
+            expect(int1.includes(int2)).toBe(expected);
+        }
+      );
+
+      let interOver1 = new Interval(0,20);
+      let interOver2 = new Interval(10,20);
+      let interOver1n = new Interval(-10,-1);
+      let interOver2n = new Interval(-10,-4);
+      let interOver3= new Interval(-5,50);
+      let interOver4= new Interval(-99,99);
+      let interOver5= new Interval(99,-99);
+  
+    // #####################################
+    // Including interval
+    // #####################################
+    test.each([
+        [interOver1, interOver2,true,""],
+        [interOver1n, interOver2n,true,""],
+        [interOver4, interOver3,true,"Test d'inclusion totale d'un intervalles par un autre"],
+        [interOver1, interOver1,true,"Test avec deux intervalles positives identiques."],
+        [interOver1n, interOver1n,true,"Test avec deux intervalles négatives identiques."],
+        [interAscii1, interAscii2,true,"Test avec des caractères ascii"],
+      ])(
+        'Test d\'Including: %s includes %s and expected(%s). %s',
+        (int1,int2,expected ) => {
+            expect(int1.includes(int2)).toBe(expected);
+        }
+      );
+
+
+});
+
